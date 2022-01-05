e9c817ebc79dc0616bafc41f56ef04c35d14d8fe
limsungchul <limsungchul@imseongcbookair>
2022-01-05 15:12:29 +0900

    gugudan 2단~5

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class Gugudan {
 
@@ -25,6 +27,43 @@ public class Gugudan {
 		System.out.println(3 * 7);
 		System.out.println(3 * 8);
 		System.out.println(3 * 9);
+		
+		// 4단 
+		System.out.println("----4단----");
+		int result = 4 * 1;
+		System.out.println(result);
+		result = 4 * 2;
+		System.out.println(result);
+		result = 4 * 3;
+		System.out.println(result);
+		result = 4 * 4;
+		System.out.println(result);
+		result = 4 * 5;
+		System.out.println(result);
+		result = 4 * 6;
+		System.out.println(result);
+		result = 4 * 7;
+		System.out.println(result);
+		result = 4 * 8;
+		System.out.println(result);
+		result = 4 * 9;
+		System.out.println(result);
+		
+		// 5단
+		System.out.println("----5단----");
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
+		
 	}
 
 }
