5f3ee2c21d760d421145f9b037482af0bf7d936a
limsungchul <limsungchul@imseongcbookair>
2022-01-12 18:50:48 +0900

    구구단 메소드를 활용해 구구단 구

--- src/gugudan_array.java
+++ src/gugudan_array.java
@@ -1,43 +1,24 @@
-import javax.print.CancelablePrintJob;
-
 public class gugudan_array {
-
-	public static void main(String[] args) {
-		
-		System.out.println("----2단----");
-		
+	public static int[] calulate(int times) {
 		int[] result = new int[9];
 		for(int i=0; i < result.length; i++) {
-			result[i] = 2 * (i +1);
+			result[i] = times * (i +1);
 		}
-		
+		return result;
+	}
+	
+	public static void print(int[] result) {
 		for(int i=0; i < result.length; i++) {
 			System.out.println(result[i]);
 		}
-		
-		System.out.println("----3단----");
-		
-		int[] result1 = new int[9];
-		for(int i=0; i < result1.length; i++) {
-			result1[i] = 3 * (i +1);
+	}
+	
+	public static void main(String[] args) {
+		for(int i = 2; i < 10; i++) {
+			System.out.println("--------");
+			int[] result = calulate(i);
+			print(result);
 		}
-		
-		for(int i=0; i < result1.length; i++) {
-			System.out.println(result1[i]);
-		}
-		
-		System.out.println("----4단----");
-		
-		int[] result2 = new int[9];
-		for(int i=0; i < result2.length; i++) {
-			result2[i] = 4 * (i +1);
-		}
-		
-		for(int i=0; i < result2.length; i++) {
-			System.out.println(result2[i]);
-		
-		}
-		
 	}
 
 }
